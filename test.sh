1e9f8ba8ab1e1339f8af70d056976a20eab148df
dave.roast <dave.roast@netreveal.ai>
2025-09-02 10:47:53 -0700

    Fixes and parameterization to upload file and check validations

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 
 CLIENT_ID=$1
 CLIENT_SECRET=$2
+BULK_FILENAME=$3
 
 RESPONSE=$(curl -X 'POST' \
   'https://www148.fintrac-canafe.canada.ca/reporting-ingest/oauth2/v2.0/token' \
@@ -10,8 +11,22 @@ RESPONSE=$(curl -X 'POST' \
   -d "client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}")
   
 ACCESS_TOKEN=$(echo "$RESPONSE" | jq -r '.access_token')
+  
+RESPONSE=$(curl -X 'GET' \
+  "https://www148.fintrac-canafe.canada.ca/reporting-ingest/api/v1/bulkSubmission?fileName=$BULK_FILENAME" \
+  -H 'accept: application/json' \
+  -H "Authorization: Bearer $ACCESS_TOKEN")
+  
+URL=$(echo "$RESPONSE" | jq -r '.payload.bulkEndpoint')
 
+curl -X 'PUT' \
+  "$URL" \
+  -H 'accept: */*' \
+  -H 'x-ms-blob-type: BlockBlob' \
+  -H 'Content-Type: application/octet-stream' \
+  --data-binary '@input.json'
+  
 curl -X 'GET' \
-  'https://www148.fintrac-canafe.canada.ca/reporting-ingest/api/v1/reports/list?pageNumber=0&pageSize=20&sortBy=createDateTime-' \
-  -H "accept: application/json" \
+  "https://www148.fintrac-canafe.canada.ca/reporting-ingest/api/v1/reports/validations?fileName=$BULK_FILENAME" \
+  -H 'accept: application/json' \
   -H "Authorization: Bearer $ACCESS_TOKEN"
