c9ffc1d16efe3da633d0399da5db0160b5867c73
dave.roast <dave.roast@netreveal.ai>
2025-09-02 09:26:20 -0700

    Fix args

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CLIENT_ID = $1
-CLIENT_SECRET = $2
+CLIENT_ID=$1
+CLIENT_SECRET=$2
 
 curl -X 'POST' \
   'https://www148.fintrac-canafe.canada.ca/reporting-ingest/oauth2/v2.0/token' \
