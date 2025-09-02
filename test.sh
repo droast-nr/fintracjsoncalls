3bdfc74edbbf9f00b6d27be5dc91fb53f8b99ae9
dave.roast <dave.roast@netreveal.ai>
2025-09-02 10:06:44 -0700

    Fixed capture of access token

--- test.sh
+++ test.sh
@@ -3,11 +3,11 @@
 CLIENT_ID=$1
 CLIENT_SECRET=$2
 
-RESPONSE=curl -X 'POST' \
+RESPONSE=$(curl -X 'POST' \
   'https://www148.fintrac-canafe.canada.ca/reporting-ingest/oauth2/v2.0/token' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/x-www-form-urlencoded' \
-  -d "client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}"
+  -d "client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}")
   
 ACCESS_TOKEN=$(echo "$RESPONSE" | jq -r '.access_token')
 
