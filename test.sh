924f1796df01b4e4c1a4c2faa4dec7779237c9c2
dave.roast <dave.roast@netreveal.ai>
2025-09-02 10:02:25 -0700

    Add in parsing of the access token and check it works with a list command

--- test.sh
+++ test.sh
@@ -3,9 +3,15 @@
 CLIENT_ID=$1
 CLIENT_SECRET=$2
 
-curl -X 'POST' \
+RESPONSE=curl -X 'POST' \
   'https://www148.fintrac-canafe.canada.ca/reporting-ingest/oauth2/v2.0/token' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -d "client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}"
   
+ACCESS_TOKEN=$(echo "$RESPONSE" | jq -r '.access_token')
+
+curl -X 'GET' \
+  'https://www148.fintrac-canafe.canada.ca/reporting-ingest/api/v1/reports/list?pageNumber=0&pageSize=20&sortBy=createDateTime-' \
+  -H "accept: application/json" \
+  -H "Authorization: Bearer $ACCESS_TOKEN"
